00e8fb443665125f7e390402f8fd6867764f9f39
ekifakhrureza <ekifakhrureza@gmail.com>
2018-03-01 22:02:07 +0700

    Add register belum kelar

--- routes/registers.js
+++ routes/registers.js
@@ -17,9 +17,36 @@ routes.post('/', function (req, res) {
     address:req.body.address,
     status:1,
   }
+  var nodemailer = require('nodemailer');
+
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      service: 'gmail',
+      auth: {
+        user: 'ekiempat@gmail.com',
+        pass: 'jacojaya'
+      }
+    });
+
+    var mailOptions = {
+      from: 'ekiempat@gmail.com',
+      to: 'ekitiga@gmail.com',
+      subject: 'Selamat Bergabung Di Jaco',
+      text: 'Klik <a href="/">Di Sini</a> Untuk Login'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
   models.User.create(obj)
   .then(data => {
-    res.redirect('/index')
+    res.redirect('/cekimel')
     // res.send(data)
   }).catch(err=>{
     res.redirect(`/Registers/?error=${err.message}`)
